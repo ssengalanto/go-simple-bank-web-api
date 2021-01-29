b175d5a398b5274074f68695708a85fca13d1111
Ssen Galanto <ssengalanto.dev@gmail.com>
2021-01-29 16:22:02 +0800

    feat: install go viper and move package const to env vars

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,19 +7,20 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
+	"github.com/ssengalanto/go-bank/util"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://postgres:postgres@localhost:5432/simple_bank?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db", err)
 	}
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/ssengalanto/go-bank/api"
 	db "github.com/ssengalanto/go-bank/db/sqlc"
+	"github.com/ssengalanto/go-bank/util"
 )
 
 const (
@@ -16,7 +17,12 @@ const (
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db", err)
 	}
@@ -24,8 +30,8 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
-		log.Fatal("Cannot start server:", err)
+		log.Fatal("cannot start server:", err)
 	}
 }
